179bb5ddeddb01e6278edcba81a9a03b52731c03
shahnshah <malik@estater.com>
2020-01-18 23:50:34 +0530

    Changes in UI

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(() => {
+    //     $('#topnav').html(`<ul>
+    //     <li>
+    //         <a class="toplink" href="/">Home</a>
+    //         <a class="toplink" href="/work">My work</a>
+    //         <a class="toplink" href="/about-shahnshah">About me</a>
+    //         <a class="toplink" href="/contact">Contact</a>
+    //     </li>
+    // </ul>`)
+})
